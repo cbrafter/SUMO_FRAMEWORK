f9823da77e92955a8e2a9c6a7a602dd352bfe05f
Craig B. Rafter <craigrafter17@gmail.com>
2018-04-26 11:03:38 +0100

    modified signalControl.py to process transitions with less API calls and tidy code

--- 1_sumoAPI/signalControl.py
+++ 1_sumoAPI/signalControl.py
@@ -80,66 +80,78 @@ class signalControl(object):
 
     
 class stageTransition(object):
-        
-        def __init__(self): 
-            self.setAmber1Time(3)
-            self.setAmber2Time(0)
-            self.setAllRedTime(1)
-            self.active=False
-            traci.simulation.subscribe(varIDs=(tc.VAR_TIME_STEP,))
-            # current+target -> amber1,amber2,allRed
-            self.transitionDict = {'rr': 'rrr', 'GG': 'GGG', 'gg': 'ggg',
-                                   'rg': 'ryr', 'Gr': 'yrr', 'gr': 'yrr',
-                                   'rG': 'ryr', 'Gg': 'Ggg', 'gG': 'gGg'}
-        
-        def setAmber1Time(self, time):
-            self.amber1Time = time
+    def __init__(self): 
+        self.setAmber1Time(3)
+        self.setAmber2Time(0)
+        self.setAllRedTime(1)
+        self.active=False
+        traci.simulation.subscribe(varIDs=(tc.VAR_TIME_STEP,))
+        # current+target -> amber1,amber2,allRed
+        self.transitionDict = {'rr': 'rrr', 'GG': 'GGG', 'gg': 'ggg',
+                               'rg': 'ryr', 'Gr': 'yrr', 'gr': 'yrr',
+                               'rG': 'ryr', 'Gg': 'Ggg', 'gG': 'gGg'}
+    
+    def setAmber1Time(self, time):
+        self.amber1Time = time
 
-        def setAmber2Time(self, time):
-            self.amber2Time = time
+    def setAmber2Time(self, time):
+        self.amber2Time = time
+    
+    def setAllRedTime(self, time):
+        self.allRed = time
         
-        def setAllRedTime(self, time):
-            self.allRed = time
-            
-        def getCurrentSUMOtime(self):
-            #return traci.simulation.getCurrentTime()
-            return traci.simulation.getSubscriptionResults()[tc.VAR_TIME_STEP]
+    def getCurrentSUMOtime(self):
+        #return traci.simulation.getCurrentTime()
+        return traci.simulation.getSubscriptionResults()[tc.VAR_TIME_STEP]
+    
+    def newTransition(self, junctionID, currentStageString, targetStageString, simtime=None):
+        if len(currentStageString) != len(targetStageString):
+            print("Error current stage string and target stage sting are different lengths")
         
-        def newTransition(self, junctionID, currentStageString, targetStageString, simtime=None):
-            if len(currentStageString) != len(targetStageString):
-                print("Error current stage string and target stage sting are different lengths")
-            
-            self.amber1StageString=""
-            self.amber2StageString=""
-            self.allRedStageString=""
-            
-            for current, target in zip(currentStageString, targetStageString):
-                transitionString = self.transitionDict[current+target]
-                self.amber1StageString += transitionString[0]
-                self.amber2StageString += transitionString[1]
-                self.allRedStageString += transitionString[2]
-            
-            self.targetStageString = targetStageString
-            self.junctionID = junctionID
-            self.transitionStart = simtime if simtime != None else self.getCurrentSUMOtime()
-            self.active = True
+        self.amber1StageString=""
+        self.amber2StageString=""
+        self.allRedStageString=""
         
-        def processTransition(self, simtime=None):             
-            if self.active:
-                simTime = simtime if simtime != None else self.getCurrentSUMOtime()
-                # First amber
-                if (simTime - self.transitionStart) < (self.amber1Time*1000):
-                    traci.trafficlights.setRedYellowGreenState(self.junctionID, self.amber1StageString)
-                # All Red
-                elif (simTime - self.transitionStart) < ((self.amber1Time + self.allRed)*1000):
-                    traci.trafficlights.setRedYellowGreenState(self.junctionID, self.allRedStageString)
-                # Second Amber
-                elif (simTime - self.transitionStart) < ((self.amber1Time + self.allRed + self.amber2Time)*1000):
-                    traci.trafficlights.setRedYellowGreenState(self.junctionID, self.amber2StageString)
-                # Target Stage
-                else:
-                    traci.trafficlights.setRedYellowGreenState(self.junctionID, self.targetStageString)
-                    self.active=False
+        for current, target in zip(currentStageString, targetStageString):
+            transitionString = self.transitionDict[current+target]
+            self.amber1StageString += transitionString[0]
+            self.amber2StageString += transitionString[1]
+            self.allRedStageString += transitionString[2]
+        
+        self.targetStageString = targetStageString
+        self.junctionID = junctionID
+        self.transitionStart = simtime if simtime != None\
+                                       else self.getCurrentSUMOtime()
+        self.active = True
+    
+    def processTransition(self, simtime=None):             
+        if self.active:
+            simTime = simtime if simtime != None else self.getCurrentSUMOtime()
+            transitionTimeDelta = simTime - self.transitionStart
+            amber1Threshold = self.amber1Time*1000
+            allRedThreshold = amber1Threshold + self.allRed*1000
+            amber2Threshold = allRedThreshold + self.amber2Time*1000
+            self.stageString = ''
+            # First amber
+            if transitionTimeDelta < amber1Threshold:
+                self.makeTransition(self.amber1StageString)
+            # All Red
+            elif transitionTimeDelta < allRedThreshold:
+                self.makeTransition(self.allRedStageString)
+            # Second Amber
+            elif transitionTimeDelta < amber2Threshold:
+                self.makeTransition(self.amber2StageString)
+            # Target Stage
+            elif transitionTimeDelta >= amber2Threshold:
+                self.makeTransition(self.targetStageString)
+                self.active=False
             else:
                 pass
-                 
+        else:
+            pass
+    
+    def makeTransition(self, transitionStageString):
+        if self.stageString != transitionStageString:
+            traci.trafficlights.setRedYellowGreenState(self.junctionID,
+                                                       transitionStageString)
+            self.stageString = transitionStageString
