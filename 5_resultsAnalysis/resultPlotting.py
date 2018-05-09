96dbb0c962d40cc948e279155b022bcad21be6cd
Craig B. Rafter <craigrafter17@gmail.com>
2018-05-09 13:52:24 +0100

    added ylim doubling, font changes, pep8, legend

--- 5_resultsAnalysis/resultPlotting.py
+++ 5_resultsAnalysis/resultPlotting.py
@@ -10,37 +10,35 @@ import numpy as np
 from numpy.matlib import repmat
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from math import ceil, log10
 
 # Use T1 fonts for plots not bitmap
 rcParams['ps.useafm'] = True
 rcParams['pdf.use14corefonts'] = True
 rcParams['text.usetex'] = True
 plt.rcParams["font.family"] = "Times New Roman"
-# We're using tex fonts so we need to bolden all text in the preambles not with fontweight='bold'
-rcParams['text.latex.preamble'] = [r'\renewcommand{\seriesdefault}{\bfdefault}']
+# We're using tex fonts so we need to bolden all text in the preambles
+# not with fontweight='bold'
+rcParams['text.latex.preamble'] = \
+    [r'\renewcommand{\seriesdefault}{\bfdefault}']
 
-tsize = 20.5
-axsize = 20.5
-ticksize = 17
+tsize = 21
+axsize = 20
+ticksize = 20
 nolab = '_nolegend_'
 
 
-def setsavefig(figure, filepath, dpiVal=300, padding=0.1,
-               pdf=True, eps=False, png=False):
+def roundUp(x, num):
+    return int(ceil(x/float(num)))*int(num)
+
+
+def savePDF(pdfFile, figure):
     ''' Save figure at publication quality using desired settings
-    
-    if png:
-        figure.savefig(filepath+'.png', dpi=dpiVal,
-                       bbox_inches='tight', pad_inches=padding)
-    if eps:
-        figure.savefig(filepath+'.eps', dpi=dpiVal,
-                       bbox_extra_artists=(lgnd,),
-                       bbox_inches='tight', pad_inches=padding)
     '''
-    if pdf:
-        figure.savefig(filepath+'.pdf', dpi=dpiVal,
-                       # bbox_extra_artists=(lgnd,),
-                       bbox_inches='tight', pad_inches=padding)
+    pdfFile.savefig(figure, dpi=300,
+                    # bbox_extra_artists=(lgnd,),
+                    bbox_inches='tight', pad_inches=0.1)
 
 
 def plotPercentile(data, scale, style='k-', alpha_val=1):
@@ -62,24 +60,21 @@ lineStyle = {'VA': '^k',
              'HVA': 'oC3'}
 
 data = pd.read_csv('/hardmem/results_test/allTripInfo.csv')
-models = ['sellyOak_avg', 'sellyOak_hi', 'sellyOak_lo']
+models = ['sellyOak_avg', 'sellyOak_lo', 'sellyOak_hi']
 controllers = ['fixedTime', 'GPSVA']
+figuresPDF = PdfPages('figures.pdf')
 
 # plot sellyOak results
 fig = plt.figure(figsize=(16, 9))
 lines = []
 labels = []
 cvp = data.groupby('cvp').delay.mean().index.values
-i=0
-for model in models:
+i = 0
+for model in models[:-1]:
     fig = plt.figure(figsize=(16, 9))
     lines = []
     labels = []
-    cvp = data.groupby('cvp').delay.mean().index.values
     for controller in controllers:
-        if model=='sellyOak_hi' and controller == 'GPSVA':
-            continue
-        
         plotData = data[(data.model == model) &
                         (data.controller == controller)]
         meanDelay = plotData.groupby('cvp').delay.mean().values
@@ -92,24 +87,43 @@ for model in models:
             meanDelay = meanDelay * np.ones_like(cvp)
             err = err * np.ones_like(cvp[:, np.newaxis])
 
-        lines.append(plt.errorbar(cvp, meanDelay, 
-                                  yerr=[meanDelay-err[:, 1],
-                                        err[:, 0]-meanDelay], 
-                                  fmt=lineStyle[controller]+'-', 
+        errLims = [meanDelay-err[:, 1], err[:, 0]-meanDelay]
+        lines.append(plt.errorbar(cvp, meanDelay,
+                                  yerr=errLims,
+                                  fmt=lineStyle[controller]+'-',
                                   linewidth=2, markersize=10,
-                                  label=controller, 
+                                  label=controller,
                                   capsize=7, capthick=2, elinewidth=1))
+        # plt.fill_between(cvp, err[:, 1], err[:, 0],
+        #                  color=lineStyle[controller][1:], alpha=0.3)
         labels.append(controller)
-        #plt.title(model+': Delay vs. CV Penetration', fontsize=tsize)
-        plt.xlabel('Percentage CV Penetration', fontsize=axsize, fontweight='bold')
+        modName = ' '.join(model.split('_'))
+        plt.title(modName+': Delay vs. CV Penetration', fontsize=tsize)
+        plt.xlabel('Percentage CV Penetration', fontsize=axsize,
+                   fontweight='bold')
         plt.ylabel('Delay [s]', fontsize=axsize)
-        plt.ylim([0,1000])
-    setsavefig(fig, './delay'+str(i))
-    i+=1
+        # order of magnitude of the max point
+        magnitude = 10**int(log10(max(meanDelay)))
+        # set x and y lims with some space around the max and min values
+        yMin = -0.3*magnitude
+        yMax = roundUp(meanDelay[0], 0.5*magnitude)
+        plt.ylim([yMin, yMax])
+        plt.xlim([-3, 103])
+        plt.xticks(cvp, fontsize=ticksize, fontweight='bold')
+        # double the ytick interval
+        newYticks = np.arange(0, yMax+1, 0.5*magnitude, dtype=int)
+        plt.yticks(newYticks, fontsize=ticksize, fontweight='bold')
+        plt.legend(labels, prop={'size': ticksize-2}, labelspacing=1)
+    savePDF(figuresPDF, fig)
+    i += 1
+figuresPDF.close()
 
-
-
-
-
-
-
+'''
+from scipy.stats import ttest_ind
+x = plotData
+for n in range(2,10):
+    d1 = x[x.run.isin(list(range(1,n)))].groupby('cvp').delay.mean().values
+    d2 = x[x.run.isin(list(range(1,n+1)))].groupby('cvp').delay.mean().values
+    t, p = ttest_ind(d1, d2)
+    print(n, t, 1-p)
+'''
