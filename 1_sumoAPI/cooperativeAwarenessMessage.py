493c54f92ae0ddd6341fedb45d14b60a1be24709
Craig B. Rafter <craigrafter17@gmail.com>
2018-05-11 14:56:05 +0100

    added ile to handle CAM related options rather than in controller directly

---
+++ 1_sumoAPI/cooperativeAwarenessMessage.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# ETSI CAM Specifications
+
+TGenCamMin = 0.1 # Min time for CAM generation 10Hz/100ms/0.1sec
+TGenCamMax = 1.0 # Max time for CAM generation 1Hz/1000ms/1sec
+TGenCamDCC = 0.1 # CAM generation time under channel congestion
+TGenCam = TGenCamMax # current upper lim for CAM generation
+NGenCamMax = 3
+Nsubcarriers = 52 # Num of 802.11p subcarriers
