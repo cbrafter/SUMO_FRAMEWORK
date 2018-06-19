1e4439f8ccde6856eec3cf5065f8d5536147028d
Craig B. Rafter <craigrafter17@gmail.com>
2018-06-19 14:42:03 +0100

    added sleep to allow time for folder creation on HPC

--- 4_simulation/simulation.py
+++ 4_simulation/simulation.py
@@ -50,11 +50,12 @@ def simulation(x):
         # Check if model copy for this process exists
         if not os.path.isdir(model):
             shutil.copytree('../2_models/{}/'.format(modelBase), model)
-            time.sleep(0.5)  # sleep to make sure files copied
+            time.sleep(2.5)  # sleep to make sure files copied
 
         # this is relative to script not cfg file
         if not os.path.exists(exportPath):
             os.makedirs(exportPath)
+            time.sleep(2.5)  # sleep to make sure files copied
 
         # Edit the the output filenames in sumoConfig
         sumoConfigGen(modelName, configFile, exportPath, 
