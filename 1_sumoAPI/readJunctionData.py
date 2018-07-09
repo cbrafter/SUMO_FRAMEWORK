946846891ebf283785505ee86b494130e193074a
Craig B. Rafter <craigrafter17@gmail.com>
2018-07-09 14:45:27 +0100

    added transyt scheme parsing

--- 1_sumoAPI/readJunctionData.py
+++ 1_sumoAPI/readJunctionData.py
@@ -9,27 +9,44 @@ Class for reading a jcn.xml file and storing junctionObj and stageObj data.
 """
 
 from xml.dom.minidom import parse
-import stageObj,junctionObj, unicodedata
+import xml.etree.ElementTree as ET
+import stageObj, junctionObj, unicodedata
 
 class readJunctionData:
     
-    def __init__(self,fileName):
-        self.dom = parse(fileName)
-    
+    def __init__(self, fileName):
+        self.filename = fileName
+        tree = ET.parse(fileName)
+        root = tree.getroot()
+        self.junctionList = root.getchildren()
+
     def getJunctionData(self):    
         junctionData = []
         
-        for jcnNode in self.dom.getElementsByTagName('junction'):
-            stageData=[]
-            for stgNode in jcnNode.getElementsByTagName('stage'):
-                id = unicodedata.normalize('NFKD',stgNode.getAttributeNode('id').nodeValue).encode('ascii','ignore')
-                controlString = unicodedata.normalize('NFKD',stgNode.getAttributeNode('controlString').nodeValue).encode('ascii','ignore')
-                period = float(unicodedata.normalize('NFKD',stgNode.getAttributeNode('period').nodeValue).encode('ascii','ignore'))
-                stageData.append(stageObj.stageObj(id, controlString, period))
+        if self.filename.split('.')[-2] != 't15':
+            for junction in self.junctionList:
+                stageData=[]
+                for stage in junction.getchildren():
+                    stageData.append(stageObj.stageObj(stage.attrib['id'],
+                                                       stage.attrib['controlString'],
+                                                       float(stage.attrib['period'])))
+                
+                junctionData.append(junctionObj.junctionObj(junction.attrib['id'],
+                                                            stageData,
+                                                            float(junction.attrib['offset'])))
+        else:
+            data = {j.attrib['id'].split('_')[0]: {} for j in self.junctionList}
+
+            for junction in self.junctionList:
+                juncID, juncMode = j.attrib['id'].split('_')
+                stageData=[]
+                for stage in junction.getchildren():
+                    stageData.append(stageObj.stageObj(stage.attrib['id'],
+                                                       stage.attrib['controlString'],
+                                                       float(stage.attrib['period'])))
+                data[juncID][juncMode] = stageData
             
-            juncID = unicodedata.normalize('NFKD',jcnNode.getAttributeNode('id').nodeValue).encode('ascii','ignore')
-            offset = float(unicodedata.normalize('NFKD',jcnNode.getAttributeNode('offset').nodeValue).encode('ascii','ignore'))
-            junctionData.append(junctionObj.junctionObj(juncID, stageData, offset))
-            
+            for jID in data.keys():   
+                junctionData.append(junctionObj.junctionObj(jID, data[jID], 0.0))
+
         return junctionData
-        
