2ae83f78e07929beed3143a82c85087bb67210bd
Craig B. Rafter <craigrafter17@gmail.com>
2018-10-12 14:20:51 +0100

    modified run configs

--- 4_simulation/simrunner.py
+++ 4_simulation/simrunner.py
@@ -38,8 +38,8 @@ configs += list(product(models[::-1], tlControllers[:1], [0.], runIDs))
 # Generate runs for CAV dependent controllers
 configs += list(product(models[:4][::-1]+models[4:],
                         tlControllers[1:], CAVratios[::-1], runIDs))
-# Test configurationsc
-onfigs = list(product(models[-3:], tlControllers[:1], CAVratios[:1], runIDs))
+# Test configurations
+configs = list(product(models[-3:], tlControllers[:1], CAVratios[:1], runIDs))
 configs += list(product(models[-3:], tlControllers[1:], CAVratios, runIDs))
 configs.sort(key=lambda x: x[2], reverse=True)
 
