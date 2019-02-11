5a93e3b2ef8025952ff7ef9b43a19e5da18c5fbd
Craig B. Rafter <craigrafter17@gmail.com>
2019-02-11 16:15:51 +0000

    testing pbs job arrays

--- 4_simulation/iridisTest.py
+++ 4_simulation/iridisTest.py
@@ -1,15 +1,19 @@
 import multiprocessing as mp
 from socket import gethostname
 import psutil as ps
+import sys
+import time
 
 def nodeTest(x):
     try:
         procID = int(mp.current_process().name.split('-')[-1])
     except:
         procID = 1
+    time.sleep(2)
     return "{:2d}: {}/{} of {}".format(x, gethostname(), procID, ps.cpu_count())
 
-workpool = mp.Pool(processes=ps.cpu_count())
-result = workpool.map(nodeTest, range(500), chunksize=1)
-result.sort(key=lambda x: int(x.split(':')[0]))
+#workpool = mp.Pool(processes=ps.cpu_count())
+#result = workpool.map(nodeTest, range(500), chunksize=1)
+#result.sort(key=lambda x: int(x.split(':')[0]))
+result = nodeTest(int(sys.argv[1]))
 print('\n'.join(result))
--- sumojob.sh
+++ sumojob.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #PBS -N CBRSUMO
 #PBS -j oe
-#PBS -l walltime=0:05:00
-#PBS -l nodes=1:ppn=16
+#PBS -l walltime=0:01:00
+#PBS -l nodes=2:ppn=16
 #PBS -m ae -M cbr1g15@soton.ac.uk
+#PBS -t 1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99,111,222,333,444,555,666,777,888,999
 # Script to run SUMO simulations 
 
 # Load required modules
