90b40bb1b25c44a5e02db0d531e3223eeb113e9b
Craig B. Rafter <craigrafter17@gmail.com>
2019-06-11 11:28:04 +0100

    added folder creating in script as python sometimes encounters errors while doing it

--- sumojob.sh
+++ sumojob.sh
@@ -4,13 +4,17 @@
 #PBS -l walltime=35:00:00
 #PBS -l nodes=1:ppn=16
 #PBS -m ae -M cbr1g15@soton.ac.uk
-#PBS -t 0-319
+#PBS -t 0-89
 
-# 0-140, 141-280, 281-319
 # Script to run SUMO simulations
-
 # Load required modules
 module load singularity/2.2.1
+
+# make model folders
+cd /scratch/cbr1g15/SUMOsingularity/SUMO_FRAMEWORK/2_models/
+for i in $(seq 0 32); do
+    cp -r sellyOak sellyOak_$(hostname)_$i;
+done
 
 # Change to the working directory
 cd /scratch/cbr1g15/SUMOsingularity/
@@ -22,4 +26,5 @@ singularity exec -w \
     sumohpc.img \
     bash -c "cd ~/sumofwk/4_simulation && python hpcrunner.py $PBS_ARRAYID"
 
+# remove model folders
 rm -rf SUMO_FRAMEWORK/2_models/sellyOak_$(hostname)_*
